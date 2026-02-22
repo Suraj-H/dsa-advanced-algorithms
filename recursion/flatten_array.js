010b24cc721d946ac9cf0ad9dfaf41342e93a8a1
SurajH <suraj.14121990@gmail.com>
2026-02-22 11:07:31 +0530

    flattenArrayInPlaceSplice

--- recursion/flatten_array.js
+++ recursion/flatten_array.js
@@ -3,13 +3,12 @@ let arr = [1, [2, 3], [4, [5, 6]]];
 // output : [1, 2, 3, 4, 5, 6]
 
 // Time O(n) & Space O(n) Complexity - Because every element is visited once.
-
-function flattenArray(arr) {
+function flattenArrayRecursion(arr) {
   let result = [];
 
   for (let num of arr) {
     if (Array.isArray(num)) {
-      result = result.concat(flattenArray(num));
+      result = result.concat(flattenArrayRecursion(num));
     } else {
       result.push(num);
     }
@@ -18,4 +17,22 @@ function flattenArray(arr) {
   return result;
 }
 
-console.log("flattenArray ->", flattenArray(arr));
+console.log("flattenArrayRecursion ->", flattenArrayRecursion(arr));
+
+// Time O(n^2) worst case scenerio & Space O(1) Complexity
+function flattenArrayInPlaceSplice(arr) {
+  let i = 0;
+
+  while (i < arr.length) {
+    // Worst case: [[[1]]]
+    if (Array.isArray(arr[i])) {
+      arr.splice(i, 1, ...arr[i]);
+    } else {
+      i++;
+    }
+  }
+
+  return arr;
+}
+
+console.log("flattenArrayInPlaceSplice ->", flattenArrayInPlaceSplice(arr));
