679f4fad242efb1abe6f53dd3fbae807ccfa7ecc
SurajH <suraj.14121990@gmail.com>
2026-02-25 21:25:29 +0530

    dsa patterns

---
+++ DSA/maximum_subarray_sum.js
@@ -0,0 +1,22 @@
+nums = [2, 1, 5, 1, 3, 2];
+k = 3;
+
+function maximumSubarraySum(nums, k) {
+  let maxSum = -Infinity;
+  let start = 0;
+  let windowSum = 0;
+
+  for (let end = 0; end < nums.length - 1; end++) {
+    windowSum += nums[end];
+
+    if (end - start + 1 === k) {
+      maxSum = Math.max(maxSum, windowSum);
+      windowSum -= nums[start];
+      start++;
+    }
+  }
+
+  return maxSum;
+}
+
+console.log("maximumSubarraySum ->", maximumSubarraySum(nums, k));
